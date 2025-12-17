2f81bb9ba9902b4fe01b44725e9131970950402b
godmayer-jdi <tig774ra@gmail.com>
2025-12-18 01:45:24 +0400

    В lms/views.py контроллеры viewset для курсов, generics для уроков

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.permissions import AllowAny
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+# ViewSet для курсов
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [AllowAny]
+
+# Generics для уроков
+class LessonListCreateView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
