aa196253a9041262ba0c7a507d6543829aeefb3c
godmayer-jdi <tig774ra@gmail.com>
2025-12-19 23:45:35 +0400

    В приложение Users добавлена новая модель Payments (Платежи). Создана фикстура для записи данных

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from lms.models import Course, Lesson
 from django.utils.translation import gettext_lazy as _
 
 
@@ -11,3 +12,17 @@ class CustomUser(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+
+
+class Payment(models.Model):
+    PAYMENT_METHODS = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, null=True, blank=True)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS)
