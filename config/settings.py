74efd5ae39ecef51062647d34e6ed9002b76a85d
godmayer-jdi <tig774ra@gmail.com>
2026-01-29 18:36:38 +0400

    Созданы nginx.conf, Dockerfile. Отредактированы docker-compose.yaml requirements.txt согласно тому как было в лекции объяснено

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-default-for-local-dev")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -171,4 +171,3 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", True)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "test@example.com")
-
